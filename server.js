bc9568081f6c3d3e55d50b278eeae471894b6604
MontesJJ <javimontes91@gmail.com>
2025-01-03 21:56:10 +0100

    FIX: Change port to fetch from local

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const passwordRoutes = require('./routes/passwordRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // Middlewares
 app.use(cors());
