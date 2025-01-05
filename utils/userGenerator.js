01d30674f323205b377f15570e285b0e047f34d7
MontesJJ <javimontes91@gmail.com>
2025-01-05 14:26:58 +0100

    FEAT: Subir diccionario de nombres y adjetivos para generar nombres de usuario aleatorios

--- utils/userGenerator.js
+++ utils/userGenerator.js
@@ -1,7 +1,20 @@
-    const generateUser = () => {
+const generateUser = () => {
 
-        return 'usuario999';
-
+    const fs = require('fs');
+    const path = require('path');
+    
+    const cargarRecursos = () => {
+        const filePath = path.join(__dirname, '../recursos.json');
+        const data = fs.readFileSync(filePath, 'utf8'); 
+        return JSON.parse(data); 
     };
 
-    module.exports = {generateUser};
+    const recursos = cargarRecursos();
+    const randomName = Math.floor(Math.random() * recursos.nombres.length);
+    const randomAdjetive = Math.floor(Math.random() * recursos.adjetivos.length);
+    const randomNumber = Math.floor(Math.random() * recursos.adjetivos.length);
+    return (recursos.nombres[randomName] + "_" + recursos.adjetivos[randomAdjetive] + "_" + randomNumber);
+
+};
+
+module.exports = { generateUser };
